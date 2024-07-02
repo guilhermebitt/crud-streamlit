c5b2085eed6384bdd5c817bf4a0c078a50bd6595
Guilherme Bittencourt <guilherme.assis.bittencourt@gmail.com>
2024-07-02 06:32:35 -0300

    Update crud-app.py
    the final version of the program

--- crud-app.py
+++ crud-app.py
@@ -6,15 +6,18 @@
 
 import requests
 import streamlit as st
+
 # STREAMLIT GUI
 st.title('Trabalho Avaliativo 05')
 st.markdown('Feito no Senac')
 st.markdown('Tutor: Antônio')
 
+error = False
+
 def verifyName(name):
     error = False
     text_error = ''
-    if name.strip().isalpha() == False or name.count(' ') >= 1:
+    if name.replace(' ', '').isalpha() == False:
         error = True
         text_error = 'O nome deve conter apenas letras.'
     return error, text_error
@@ -25,10 +28,10 @@ def verifyPhone(telephone):
     text_error = ''
     if telephone.isnumeric() == False:
         error = True
-        text_error = 'O telefone deve conter apenas números.'
-    if len(telephone) != 10:
+        text_error = 'O telefone deve conter números.'
+    if len(telephone) != 10 and len(telephone) != 11:
         error = True
-        text_error = 'O telefone deve conter apenas dez digitos.'
+        text_error = 'O telefone deve conter dez digitos.'
     return error, text_error
 
 
@@ -59,28 +62,61 @@ def verifyCpf(cpf):
 
 
 def lookCep(cep):
+    error = False
     try:
         url = f'https://viacep.com.br/ws/{cep}/json/'
         response = requests.get(url)
         dados = response.json()
+        if 'erro' in dados:
+            text_error = 'CEP inexistente'
+            error = True
+            return error, text_error
     except:
-        error = f'CEP inválido ou inexistente. Código de erro: {response.status_code}'
-        return error
+        text_error = f'CEP inválido. Código de erro: {response.status_code}'
+        error = True
+        return error, text_error
     else:
-        return dados # logradouro -> rua / localidade -> cidade
+        return error, dados # logradouro -> rua / localidade -> cidade
 
 
-name = str(input('Nome: ')).strip()
-telephone = str(input('Telephone: ')).replace(' ', '')
-email = str(input('E-mail: ')).strip()
-cpf = str(input('CPF: ')).strip()
-cep = str(input('CEP: ')).strip()
+name = str(st.text_input(label='Nome')).strip()
+error_name = verifyName(name)
+if name and error_name[0] == True:
+    st.error(error_name[1])
+    error = True
 
-test = []
-test.append(verifyName(name))
-test.append(verifyPhone(telephone))
-test.append(verifyEmail(email))
-test.append(verifyCpf(cpf))
-test.append(lookCep(cep))
+telephone = str(st.text_input(label='Telefone')).replace(' ', '')
+error_tel = verifyPhone(telephone)
+if telephone and error_tel[0] == True:
+    st.error(error_tel[1])
+    error = True
 
-st.write(test)
+email = str(st.text_input(label='E-mail')).strip()
+error_email = verifyEmail(email)
+if email and error_email[0] == True:
+    st.error(error_email[1])
+    error = True
+
+cpf = str(st.text_input(label='CPF')).strip()
+error_cpf = verifyCpf(cpf)
+if cpf and error_cpf[0] == True:
+    st.error(error_cpf[1])
+    error = True
+
+cep = str(st.text_input(label='CEP')).strip()
+error_cep = lookCep(cep)
+if cep and error_cep[0] == True:
+    st.error(error_cep[1])
+    error = True
+else:
+    address = lookCep(cep)[1]
+
+if name and telephone and email and cpf and cep and error == False:
+    dados = f'''Nome: {name}
+telefone: {telephone}
+E-mail: {email}
+CPF: {cpf}
+Endereço: Cidade: {address['localidade']} | Bairro {address['bairro']} | Rua: {address['logradouro']}
+'''
+
+    st.download_button('Exportar Informações', str(dados), 'user-info.txt')
